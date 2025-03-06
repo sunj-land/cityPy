eba6d98f2754fba9606029f9d13bab15f3b9b8a1
sunj-land <mr960408@gmail.com>
2025-03-06 23:04:15 +0800

    feat: 初始化工程

---
+++ luffyapi/luffyapi/settings/__init__.py
@@ -0,0 +1,3 @@
+SECRET_KEY = "django-insecure-2%1-h@8#%3&8we&6oi^+w*n@@#&7j0j4@grasqkk8u^%_(yu0+"
+DEBUG = True
+ROOT_URLCONF = "luffyapi.urls"
---
+++ luffyapi/manage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+# 添加虚拟环境第三包路径
+sys.path.append('./sunjPyEnv/lib/python3.12/site-packages')
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "luffyapi.settings.dev")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
